e48e486f70c31950f88445b06778dc3e7816bf67
naveen8794 <naveen8794@gmail.com>
2020-02-26 13:47:17 -0500

    Formatting code

--- Service/GetSpotifyPlaylists.py
+++ Service/GetSpotifyPlaylists.py
@@ -3,6 +3,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 from Configs.SpotifyConfig import CLIENT_ID, CLIENT_SECRET, USER
 
+
+def get_playlists():
+    """client credentials flow which doesnt require redirect URI"""
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists(USER)
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("Playlist-%d : URI - %s ,Playlist_Name - %s." % (
+            i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
+
 # Authorization Code flow required for redirecting app
 
 # token = util.prompt_for_user_token(USER,client_id=CLIENT_ID, client_secret=CLIENT_SECRET,scope='user-library-read',redirect_uri= REDIRECT_URI)
@@ -15,18 +33,3 @@ from Configs.SpotifyConfig import CLIENT_ID, CLIENT_SECRET, USER
 #         print(track['name'] + ' - ' + track['artists'][0]['name'])
 # else:
 #     print("Can't get token for", USER)
-
-
-#### client credentials flow which doesnt require redirect URI
-
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-playlists = sp.user_playlists(USER)
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
